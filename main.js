606ac3627def19e9cf1f0b2c0ca6200e1347868c
Marz270 <nicomars270@gmail.com>
2025-02-04 03:38:08 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-onload = () =>{
+onload = () => {
     document.body.classList.remove("container");
 };
 
 document.addEventListener('click', function() {
-    window.location.href = 'https://www.youtube.com/watch?v=Jf6RaP2aHVA';
+    window.open('https://www.youtube.com/watch?v=Jf6RaP2aHVA', '_blank')
 });
