b0a09373c744031e74673f36f82de55ffe2ffabc
Marz270 <nicomars270@gmail.com>
2025-02-04 03:32:57 -0300

    link

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+document.addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=Jf6RaP2aHVA';
+});
